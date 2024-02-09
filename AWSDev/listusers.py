b1e1e0fcd778a250b9e5195db9dd1369430b2949
CodingFoDummies <140344788+CodingFoDummies@users.noreply.github.com>
2024-02-09 15:50:12 -0500

    Getting the list of users
    Getting the list of users

--- AWSDev/listusers.py
+++ AWSDev/listusers.py
@@ -1,2 +1,19 @@
 import boto3
-from botocore
+
+aws_management_console = boto3.Session(profile_name="default")
+iam_console = aws_management_console.resource('iam')
+
+for each_user in iam_console.users.all():
+    print(each_user.name)
+
+aws_management_console = boto3.Session(profile_name='default')
+iam_console_client = aws_management_console.client('iam')
+
+for each_user in iam_console_client.list_users()['Users']:
+    print(each_user['UserName'])
+
+    
+
+
+
+
