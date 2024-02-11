a444ef60fa034334b461461f1d8f679dcf357d41
CodingFoDummies <140344788+CodingFoDummies@users.noreply.github.com>
2024-02-11 13:08:06 -0500

    changes to write tests

--- AWSDev/EC2Class.py
+++ AWSDev/EC2Class.py
@@ -3,32 +3,57 @@ import boto3
 class EC2Class:
     region = 'us-east-2'
     service_name = 'ec2'
-    machine_type = 't2.micro'
-    ImageId = 'ami-0c20d88b0021158c6',
+    instance_type = 't2.micro'
+    ImageId = 'ami-0c20d88b0021158c6'
+    min_count = 1
+    max_count = 1
 
+   
     def __init__(self, region, service_name, machine_type, ImageId):
         self.region = region
         self.service_name = service_name
         self.machine_type = machine_type
         self.ImageId = ImageId
+
+
+    def create_ec2_instances()->None:
+        try:
+            ec2_resource = boto3.client('ec2')
+            print("creating EC2 instances")
+            
+            result = ec2_resource.run_instances(
+                ImageId = 'ami-0c20d88b0021158c6',
+                min_count = 1,
+                max_count = 1,
+                instance_type = 't2.micro'
+                )
+            return result
+        except Exception as e:
+            print(e)
     
 
-Ec1 = EC2Class('us-west-1', 'ec1', 't3.micro', '1111234444')
 
-Ec22 = EC2Class('us-west-22', 'ec22', 't3.micro', '222234444')
+ 
 
-print("Ec1 details")
-print('ec1 region is ', Ec1.region)
+Ec1 = EC2Class('us-west-1', 'ec2', 't2.micro', 'ami-0c20d88b0021158c6')
 
+# Ec22 = EC2Class('us-west-22', 'ec22', 't3.micro', '222234444')
 
-print("Ec22 details")
-print('ec22 region is ', Ec22.region)
-#global
-
-print("global region is ", EC2Class.region)
+# print("Ec1 details")
+# print('ec1 region is ', Ec1.region)
 
 
-# ec2_instance = boto3.client(service_name='ec2', region_name='us-east-2')
+# print("Ec22 details")
+# print('ec22 region is ', Ec22.region)
+# #global
+
+# print("global region is ", EC2Class.region)
+
+#creating an instance
+result = Ec1.create_ec2_instances()
+
+
+#ec2_instance = boto3.client(service_name='ec2', region_name='us-east-2')
 
 
 
--- AWSDev/list_ec2_instances.py
+++ AWSDev/list_ec2_instances.py
@@ -11,3 +11,11 @@ aws_management_console = boto3.Session(profile_name="default",region_name='us-ea
 ec2_console = aws_management_console.client(service_name='ec2')
 result= ec2_console.describe_instances()['Reservations']
 print(result)
+
+
+# response = ec2_instance.run_instances(
+#         ImageId = 'ami-0c20d88b0021158c6',
+#         InstanceType = 't2.micro',
+#         MaxCount = 1,
+#         MinCount =1
+# )
